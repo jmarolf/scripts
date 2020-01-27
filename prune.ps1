e9b88ef0e1250c248699be8edb36398700db4e4d
Jonathon Marolf <jmarolf@microsoft.com>
2020-01-27 09:09:47 -0800

    update prune script for case where local branches have been deleted

--- prune.ps1
+++ prune.ps1
@@ -4,7 +4,13 @@ Param(
 
 try {
     & git fetch --all --prune
-    git branch --merged upstream/master --no-color | grep -v \* |  % { & git branch -D $_.Trim() }
+    git branch -r --merged upstream/master --no-color | grep -v \* |  % { 
+      $strVal = $_.Trim()
+      if($strVal -like 'origin/*') {
+          & git push -d origin ($strVal -split "/", 2)[1]
+      }
+    }
+    & git fetch --all --prune
 }
 catch {
   Write-Host $_.ScriptStackTrace
