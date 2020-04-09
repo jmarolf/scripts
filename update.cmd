feba73f6577c84a0145b1738c6ddf663b75a0725
Jonathon Marolf <jmarolf@microsoft.com>
2020-04-09 07:22:11 -0700

    fixup

--- update.cmd
+++ update.cmd
@@ -1,6 +1,6 @@
 
+powershell -noprofile -executionPolicy Unrestricted  -file "%~dp0update.ps1"
 call update-vs.bat
 choco upgrade all -y
 scoop update
 scoop update *
-powershell -noprofile -executionPolicy Unrestricted  -file "%~dp0update.ps1"
