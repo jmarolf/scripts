facbae25d82751e6ff270fc811789edcd54610c9
Jonathon Marolf <jmarolf@microsoft.com>
2020-09-28 09:36:12 -0700

    add patching for 32 core machines

---
+++ speedupAll.ps1
@@ -0,0 +1,4 @@
+.\SpeedUp.ps1 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise' -check
+.\SpeedUp.ps1 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview' -check
+.\SpeedUp.ps1 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Int.Preview' -check
+.\SpeedUp.ps1 'C:\Program Files (x86)\Microsoft Visual Studio\2019\int.main' -check
--- update.cmd
+++ update.cmd
@@ -4,3 +4,5 @@ call update-vs.bat
 choco upgrade all -y
 scoop update
 scoop update *
+
+powershell -noprofile -executionPolicy Unrestricted  -file "%~dp0speedupAll.ps1"
