6a5fdda41cc2f21276f350639b0b515ebce0595a
Jonathon Marolf <jmarolf@microsoft.com>
2021-09-28 16:03:55 -0700

    don't update these apps

--- update.ps1
+++ update.ps1
@@ -3,6 +3,12 @@ $installedPrograms = Get-Content -Raw -Path installedPrograms.json | ConvertFrom
 $installedPackages= ($installedPrograms.Sources | Select-Object -Property Packages).Packages 
 $installedPackages 
     | Where-Object {$_.PackageIdentifier -notlike "Microsoft.Teams"}
+    | Where-Object {$_.PackageIdentifier -notlike "Microsoft.VisualStudio.2022*"}
+    | Where-Object {$_.PackageIdentifier -notlike "Microsoft.VisualStudio.2019*"}
+    | Where-Object {$_.PackageIdentifier -notlike "NZXT.CAM"}
+    | Where-Object {$_.PackageIdentifier -notlike "b1f79681"}
+    | Where-Object {$_.PackageIdentifier -notlike "BlueMicrophones.BlueSherpa"}
     | ForEach-Object { 
         winget upgrade --id $_.PackageIdentifier
     }
+Remove-Item installedPrograms.json
